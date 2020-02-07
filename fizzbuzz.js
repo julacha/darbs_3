0af164e13e6fdeec491e98115060cce7da02f48c
Julia.Zorina <julia.zorinaa@gmail.com>
2020-02-07 23:58:26 +0200

    add js

---
+++ fizzbuzz.js
@@ -0,0 +1,13 @@
+console.log("Started FizzBuzz");
+
+function addListeners() {
+   console.log("Adding Listeners");
+   
+}
+
+function main (){
+    console.log("Running main");
+    addListeners();
+}
+
+main();
