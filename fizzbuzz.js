05eb3fddd8d2f7c1e4226253533bbef14e71d812
Julia.Zorina <julia.zorinaa@gmail.com>
2020-02-08 14:47:00 +0200

    add event

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,8 +1,9 @@
 console.log("Started FizzBuzz");
 const cell = document.getElementsByClassName("cell");
 
-function onClick(){
-console.log("Clicked some button");
+function onClick(event) {
+    console.log("Event of type", event.type);
+    console.log("My id is", event.currentTarget.id);
 }
 
 
@@ -10,13 +11,10 @@ function addListeners() {
    console.log("Adding Listeners");
    console.log(cell.length);
 for (let i=0; i < cell.length; i++){
-    const cl = cell[i];
-    console.log(cl.id);
+    const cl = cell[i]; 
+    //console.log(cl.id);
     cl.addEventListener("click", onClick);
-
 }
-
-
 }
 
 function main (){
