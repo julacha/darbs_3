18d431954d446d3721fcf452f1a877018927d458
Julia.Zorina <julia.zorinaa@gmail.com>
2020-02-13 13:09:17 +0200

    added changes

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -58,18 +58,20 @@ function onShowAllFizzBuzz () {
     allFizzBuzzCells[i].classList.add("fizzbuzz_selected");
     }
 } 
-function onCreate(){
-    
-}
+/* function onCreate(){
+
+} */
 
 function onClearAll() {
-    console.log (`My starting value is ${minInp.value}`);
-    console.log (`My ending Value is ${maxInp.value}`);
-for(let i=0; i < document.getElementsByClassName('cell').length; i++){
-    //const sq = cell [i];
+console.log (`My starting value is ${minInp.value}`);
+console.log (`My ending Value is ${maxInp.value}`);    
+
+   
+for(let i=0; i < cell.length; i++){
+    const cell = cell [i];
     if (cell < minInp && maxInp < cell){ 
-    document.getElementsByClassName('cell')[i].remove();   
-    //sq[i].classList.remove(cell[i]); 
+    document.getElementsByClassName('cell').remove(cell[i]);   
+    //cell[i].classList.remove(cell); 
  } 
  console.log("Clear other elements");  
 }
